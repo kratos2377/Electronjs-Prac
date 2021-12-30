d01ddbe0173fa90a0302b14c3f6f056708a011a8
SHOBHIT YADAV <shobityadav23@gmail.com>
2021-12-30 06:53:59 -0800

    Sys Top Done

--- SysTop/src/index.js
+++ SysTop/src/index.js
@@ -1,46 +1,104 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const path = require("path");
+const { app, Menu, ipcMain } = require("electron");
+const Store = require("./Store");
+const MainWindow = require("./MainWindow");
+const AppTray = require("./AppTray");
+const { dirname } = require("path");
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+// Set env
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
+let mainWindow;
+let tray;
+
+// Init store & defaults
+const store = new Store({
+  configName: "user-settings",
+  defaults: {
+    settings: {
+      cpuOverload: 80,
+      alertFrequency: 5,
+    },
+  },
+});
+
+function createMainWindow() {
+  mainWindow = new MainWindow(`file://${__dirname}/index.html`, isDev);
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+app.on("ready", () => {
+  createMainWindow();
+
+  mainWindow.webContents.on("dom-ready", () => {
+    mainWindow.webContents.send("settings:get", store.get("settings"));
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+  mainWindow.on("close", (e) => {
+    if (!app.isQuitting) {
+      e.preventDefault();
+      mainWindow.hide();
+    }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+    return true;
+  });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+  const icon = path.join(__dirname, "assets", "icons", "tray_icon.png");
+
+  // Create tray
+  tray = new AppTray(icon, mainWindow);
+});
+
+const menu = [
+  ...(isMac ? [{ role: "appMenu" }] : []),
+  {
+    role: "fileMenu",
+  },
+  {
+    label: "View",
+    submenu: [
+      {
+        label: "Toggle Navigation",
+        click: () => mainWindow.webContents.send("nav:toggle"),
+      },
+    ],
+  },
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
+];
+
+// Set settings
+ipcMain.on("settings:set", (e, value) => {
+  store.set("settings", value);
+  mainWindow.webContents.send("settings:get", store.get("settings"));
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
     app.quit();
   }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createMainWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+app.allowRendererProcessReuse = true;
